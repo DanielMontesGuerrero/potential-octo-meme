688be496d623d6f9a3f6b8a530e78c53b718e019
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2022-01-26 20:43:12 -0600

    Arreglar errores de prettier

--- megumin/assets/ColorSchema.js
+++ megumin/assets/ColorSchema.js
@@ -1,39 +1,39 @@
 const ColorSchema = {
-	red: {
-		light: '#FF8589',
-		normal: '#FF595E',
-		dark: '#FF0A12',
-	},
-	green: {
-		light: '#A1DB43',
-		normal: '#8AC926',
-		dark: '#5F891A',
-	},
-	yellow: {
-		light: '#FFD970',
-		normal: '#FFCA3A',
-		dark: '#F5B400',
-	},
-	blue: {
-		light: '#269BE3',
-		normal: '#1982C4',
-		dark: '#10547E',
-	},
-	purple: {
-		light: '#7E5DAC',
-		normal: '#6A4C93',
-		dark: '#44315E',
-	},
-	background: {
-		light: '#333333',
-		normal: '#1B1B1B',
-		dark: '#0A0A0A',
-	},
-	light: {
-		light: '#EFEFEF',
-		normal: '#D6D6D6',
-		dark: '#ADADAD',
-	},
+  red: {
+    light: '#FF8589',
+    normal: '#FF595E',
+    dark: '#FF0A12',
+  },
+  green: {
+    light: '#A1DB43',
+    normal: '#8AC926',
+    dark: '#5F891A',
+  },
+  yellow: {
+    light: '#FFD970',
+    normal: '#FFCA3A',
+    dark: '#F5B400',
+  },
+  blue: {
+    light: '#269BE3',
+    normal: '#1982C4',
+    dark: '#10547E',
+  },
+  purple: {
+    light: '#7E5DAC',
+    normal: '#6A4C93',
+    dark: '#44315E',
+  },
+  background: {
+    light: '#333333',
+    normal: '#1B1B1B',
+    dark: '#0A0A0A',
+  },
+  light: {
+    light: '#EFEFEF',
+    normal: '#D6D6D6',
+    dark: '#ADADAD',
+  },
 };
 
 export default ColorSchema;
